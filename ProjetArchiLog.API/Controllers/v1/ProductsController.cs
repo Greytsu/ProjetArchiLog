3de12106dd09cb5a8ccbfbfdf082784321d2264e
main <strohllucas93160@gmail.com>
2022-01-21 11:32:43 +0100

    versioning

---
+++ ProjetArchiLog.API/Controllers/v1/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetArchiLog.API.Data;
+using ProjetArchiLog.API.Models;
+using ProjetArchiLog.Library.Controllers.v1;
+
+namespace ProjetArchiLog.API.Controllers.v1
+{
+    public class ProductsController : BaseController<ArchiDbContext, Product>
+    {
+        public ProductsController(ArchiDbContext context) : base(context)
+        {
+        }
+
+    }
+}
